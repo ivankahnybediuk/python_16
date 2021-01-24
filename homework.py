e9fd8d7eb10c9526da0f6a308843ae4f360d2866
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2021-01-24 14:05:24 +0300

    /

---
+++ homework.py
@@ -0,0 +1,5 @@
+"""
+Task 1
+Create your own implementation of a built-in function enumerate, named `with_index`, which takes two parameters:
+`iterable` and `start`, default is 0. Tips: see the documentation for the enumerate function
+"""
